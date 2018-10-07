ce9f20cf1ef34009c63c822bc9049ccefb95b09c
Peter Schindler <petertasschindler@gmail.com>
2018-10-07 21:11:54 +0200

    encoding

--- api/getToken.js
+++ api/getToken.js
@@ -1,4 +1,9 @@
 module.exports = () => {
+  const { IDEALISTA_API_KEY, IDEALISTA_API_SECRET } = process.env;
+  const endcodedKeySecret = Buffer.from(`${IDEALISTA_API_KEY}:${IDEALISTA_API_SECRET}`).toString('base64');
+  console.log(IDEALISTA_API_KEY);
+  console.log(IDEALISTA_API_SECRET);
+  console.log(endcodedKeySecret);
   const token = 'toooken';
   return token;
 };
--- index.js
+++ index.js
@@ -12,5 +12,3 @@ const x2 = func2(x1);
 
 // STEP 3: Write to Database
 const x3 = func3(x2);
-
-console.log(x3);
