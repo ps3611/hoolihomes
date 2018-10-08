1f98a3465f93ac814d0c6a279a01b90acbb75019
Peter Schindler <petertasschindler@gmail.com>
2018-10-08 13:02:23 +0200

    feat: added mongo connection

--- db/db.js
+++ db/db.js
@@ -1,3 +1,12 @@
-// DB WRITE GOES HERE...
+const mongoose = require('mongoose');
 
-module.exports = x => x + 10;
+mongoose.connect(
+  'mongodb://localhost:27017/mth',
+  { useNewUrlParser: true },
+  () => console.log('Connected to mth MongoDB with Mongoose!'),
+);
+
+const db = mongoose.connection;
+db.on('error', err => console.log(err));
+
+module.exports = db;
