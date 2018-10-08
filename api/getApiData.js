67a5e5805bc149fb6e172f26c1538bb30c1d1c39
Peter Schindler <petertasschindler@gmail.com>
2018-10-08 10:21:14 +0200

    adding property list fetching

--- api/getApiData.js
+++ api/getApiData.js
@@ -1,8 +1,18 @@
-// API FETCH GOES HERE
+const fetch = require('node-fetch');
 const getToken = require('./getToken');
 
 module.exports = async () => {
-  const token = await getToken();
-  console.log(token);
-  return token;
+  const accessToken = await getToken();
+
+  const response = await fetch('https://api.idealista.com/3.5/es/search?operation=sale&propertyType=homes&center=40.123,-3.242&distance=100000', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const responseJson = await response.json();
+  console.log(responseJson);
+  return 1;
 };
