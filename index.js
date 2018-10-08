218ae135501989be2885362025b30b481bfc1a02
Peter Schindler <petertasschindler@gmail.com>
2018-10-08 11:42:12 +0200

    remove console log

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // THIS IS EXECUTED PERIODICALLY BY CRON
 require('dotenv').load();
-const func1 = require('./api/getApiData');
+const getApiData = require('./api/getApiData');
 const func2 = require('./algorithm/algorithm');
 const func3 = require('./db/db');
 
-// STEP 1: Fetch API Data
-const x1 = func1();
+async function main() {
+  // STEP 1: Fetch API Data
+  const listings = await getApiData();
+  // STEP 2: Apply Algorythm
+  const x2 = func2(listings);
+  // STEP 3: Write to Database
+  const x3 = func3(x2);
+}
 
-// STEP 2: Apply Algorythm
-const x2 = func2(x1);
 
-// STEP 3: Write to Database
-const x3 = func3(x2);
+main();
