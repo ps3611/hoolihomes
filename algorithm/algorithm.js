c8b31aa5a12d0b19ff60f243a9da63f596769b2a
Peter Schindler <petertasschindler@gmail.com>
2018-10-09 14:37:11 +0200

    refactor: extract average ppsm as function

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,13 +1,18 @@
 const loadRawDataFromDb = require('../db/loadRawDataFromDb');
 
+function avgCalc(homesArray) {
+  const m2PriceArray = homesArray.map(obj => obj.priceByArea);
+  const avgM2Price = m2PriceArray.reduce((a, b) => a + b) / m2PriceArray.length;
+  return avgM2Price;
+}
+
 function diffCalc(realPrice, estimatePrice) {
   return Math.round(100 * (estimatePrice - realPrice) / realPrice);
 }
 
 module.exports = async () => {
   const rawData = await loadRawDataFromDb();
-  const m2PriceArray = rawData.map(obj => obj.priceByArea);
-  const avgM2Price = m2PriceArray.reduce((a, b) => a + b) / m2PriceArray.length;
+  const avgM2Price = avgCalc(rawData);
   const formattedData = rawData.map((obj) => {
     const formattedObj = {};
     formattedObj.thumbnail = obj.thumbnail;
