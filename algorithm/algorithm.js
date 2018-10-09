45c0e7a8c32a01b9e4621d0c821e595e87b13966
Peter Schindler <petertasschindler@gmail.com>
2018-10-09 15:05:59 +0200

    refactor: move helper functions to separate file

--- algorithm/algorithm.js
+++ algorithm/algorithm.js
@@ -1,15 +1,6 @@
 const loadRawDataFromDb = require('../db/loadRawDataFromDb');
 const writeFormattedDataToDb = require('../db/writeFormattedDataToDb');
-
-function avgCalc(homesArray) {
-  const m2PriceArray = homesArray.map(obj => obj.priceByArea);
-  const avgM2Price = m2PriceArray.reduce((a, b) => a + b) / m2PriceArray.length;
-  return avgM2Price;
-}
-
-function diffCalc(realPrice, estimatePrice) {
-  return Math.round(100 * (estimatePrice - realPrice) / realPrice);
-}
+const { avgCalc, diffCalc } = require('./helpers');
 
 module.exports = async () => {
   const rawData = await loadRawDataFromDb();
