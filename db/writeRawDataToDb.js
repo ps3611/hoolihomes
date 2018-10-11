f99d206a81ca69e37dbf6666e3ea76bdb33608e8
Peter Schindler <petertasschindler@gmail.com>
2018-10-11 16:28:01 +0200

    refactor: remove dead code

--- db/writeRawDataToDb.js
+++
@@ -1,9 +0,0 @@
-const RawHome = require('./model/rawHome');
-const connection = require('./db');
-
-module.exports = async (rawHomesList) => {
-  const dropResponse = await connection.collections.rawhomes.drop();
-  console.log(dropResponse ? 'drop successful!' : 'drop error!');
-  const insertManyResponse = await RawHome.insertMany(rawHomesList);
-  console.log(insertManyResponse.length > 0 ? 'insertMany successful!' : 'insertMany error!');
-};
