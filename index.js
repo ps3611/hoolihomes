6adecbc5d92cfb763ee537a063dd0ff4d7fcaa4a
Peter Schindler <petertasschindler@gmail.com>
2018-10-11 18:41:15 +0200

    feat: drop rawhomes at end of process

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 require('dotenv').load();
 const runApiService = require('./api');
 const algorithm = require('./algorithm');
+const connection = require('./db/db');
 
 async function main() {
   // STEP 1: Get data from different APIs
-  // await Promise.all(runApiService());
+  await Promise.all(runApiService());
 
   // STEP 2: Apply algorithm
   await algorithm();
 
-  // shut down Mongo connection.
-  process.exit();
+  // finalizing steps
+  try {
+    await connection.collections.rawhomes.drop();
+    process.exit();
+  }
+  catch (err) {
+    console.log('Shutdown ERROR', err);
+  }
 }
 
 main();
