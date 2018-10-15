bfbc869a5e30c1f1773957b6d9e28dd44467f6a3
Peter Schindler <petertasschindler@gmail.com>
2018-10-15 18:27:04 +0200

    refactor: saveFormattedData to its own file

--- algorithm/helpers.js
+++ algorithm/helpers.js
@@ -1,6 +1,4 @@
-const connection = require('../db/db');
 const RawHome = require('../db/model/rawHome');
-const Home = require('../db/model/home');
 
 module.exports.loadRawData = async () => {
   try {
@@ -8,32 +6,6 @@ module.exports.loadRawData = async () => {
   }
   catch (e) {
     console.log(e);
-  }
-};
-
-module.exports.saveFormattedData = async (formattedHomesArray) => {
-  try {
-    const rawhomes = await connection.collections.rawhomes.find({}).toArray();
-    const rawhomesId = new Set(rawhomes.map(obj => obj.id));
-    await connection.collections.rawhomes.drop();
-
-    const homes = await connection.collections.homes.find({}).toArray();
-    const homesId = new Set(homes.map(obj => obj.id));
-
-    // ADD
-    const addIds = [...rawhomesId].filter(id => !homesId.has(id));
-    const homesToAdd = formattedHomesArray.filter(home => addIds.includes(home.id));
-    await Home.insertMany(homesToAdd);
-    // UPDATE
-    const updateIds = [...homesId].filter(id => rawhomesId.has(id));
-    const homesToUpdate = formattedHomesArray.filter(home => updateIds.includes(home.id));
-    await Home.updateMany({ id: { $in: updateIds } }, homesToUpdate);
-    // DELETE
-    const deleteIds = [...homesId].filter(id => !rawhomesId.has(id));
-    await Home.deleteMany({ id: { $in: deleteIds } });
-  }
-  catch (err) {
-    console.log('Shutdown ERROR', err);
   }
 };
 
