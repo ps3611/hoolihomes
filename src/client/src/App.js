581566a9ccf473666530022602947d3f92032c81
Peter Schindler <petertasschindler@gmail.com>
2018-12-21 17:55:27 +0100

    feat: basic app components

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1>navbar</h1>
+        </div>
+        <div>
+          <h1>listview</h1>
+          <h1>mapview</h1>
+        </div>
       </div>
     );
   }
