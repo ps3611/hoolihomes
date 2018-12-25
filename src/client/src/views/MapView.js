34d321cfca69ddb1c4afeba9200672459e5273d1
Peter Schindler <petertasschindler@gmail.com>
2018-12-25 19:28:49 +0100

    feat: initialize google maps

--- src/client/src/views/MapView.js
+++ src/client/src/views/MapView.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Map from './Map';
 import '../styles/MapView.css'
+
+const API_KEY = 'AIzaSyC0u6q2foaWzf3PEJWi9pKWDTnXpL9ENMc';
 
 class MapView extends Component {
   render() {
     return (
       <div className="MapView">
-        <h1>Map View</h1>
+        <Map
+  				doctors={this.props.doctors}
+  				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+  				loadingElement={<div style={{ height: `100%` }} />}
+  				containerElement={<div style={{ height: `100%`, width: `100%` }} />}
+  				mapElement={<div style={{ height: `100%` }} />}
+  			/>
       </div>
     );
   }
