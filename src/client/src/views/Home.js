7aceeae71ad08323a824d8ba125402fb5a0f8896
Peter Schindler <petertasschindler@gmail.com>
2018-12-25 13:28:55 +0100

    style: home layout

--- src/client/src/views/Home.js
+++ src/client/src/views/Home.js
@@ -14,11 +14,11 @@ class Home extends Component {
         </div>
         <div className='HomeInfo'>
           <div className='HomeTitle'>
-            <h3>Home Name</h3>
+            <h3 style={{backgroundColor:'red'}}>Home Name</h3>
           </div>
           <div className='HomeDetails'>
-            <p>{size}m2</p>
-            <p>${price}</p>
+            <p style={{backgroundColor:'red'}}>{size}m2</p>
+            <p style={{backgroundColor:'red'}}>${price}</p>
           </div>
         </div>
       </div>
