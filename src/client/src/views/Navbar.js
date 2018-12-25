37e87a519fe96a124efee97ae3d5730bc0bb331f
Peter Schindler <petertasschindler@gmail.com>
2018-12-25 13:10:26 +0100

    feat: navbar filter layout

--- src/client/src/views/Navbar.js
+++ src/client/src/views/Navbar.js
@@ -5,7 +5,26 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <h1>Hooli Homes</h1>
+        <div className="NavbarLogo">
+          <h1>Hooli Homes</h1>
+        </div>
+        <div className="NavbarFilters">
+          <div className="Filter">
+            <h3>Price Filter</h3>
+          </div>
+          <div className="Filter">
+            <h3>Price Per M2</h3>
+          </div>
+          <div className="Filter">
+            <h3>Size</h3>
+          </div>
+          <div className="Filter">
+            <h3>City Filter</h3>
+          </div>
+          <div className="Filter">
+            <h3>Country Filter</h3>
+          </div>
+        </div>
       </div>
     );
   }
