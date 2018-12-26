24f5d6bc35114b8cdfe7345cb1b640a87d1c81f2
Peter Schindler <petertasschindler@gmail.com>
2018-12-26 12:14:24 +0100

    feat: add styled markers

--- src/client/src/views/HomeMarker.js
+++ src/client/src/views/HomeMarker.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { Marker } from 'react-google-maps';
 
 export default class HomeMarker extends React.Component {
-  render(){
+  render() {
+    const icon = {
+      url: require('../assets/MarkerGreen.svg'),
+      scaledSize: {
+        width: 52,
+        height: 52,
+      }
+    };
     return(
         <Marker
           position={this.props.location}
+          label={'200kE'}
+          options={{
+            icon: icon
+          }}
         >
         </Marker>
     );
