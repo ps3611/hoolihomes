908a75f70f58e4149844ce77af95248ad1736ac6
Peter Schindler <petertasschindler@gmail.com>
2018-12-26 22:59:17 +0100

    feat: initialize filter popup

--- src/client/src/views/Navbar.js
+++ src/client/src/views/Navbar.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
+import ToolTip from 'react-portal-tooltip';
 import '../styles/Navbar.css'
 
 class Navbar extends Component {
+
+  state = {
+     isTooltipActive: false
+  }
+
+  showTooltip() {
+     this.setState({isTooltipActive: true})
+  }
+
+  hideTooltip() {
+     this.setState({isTooltipActive: false})
+  }
+
   render() {
     return (
-      <div className="Navbar">
-        <div className="NavbarLogo">
+      <div className='Navbar'>
+        <div className='NavbarLogo'>
           <h1>Hooli Homes</h1>
         </div>
-        <div className="NavbarFilters">
-          <div className="Reset">
+        <div className='NavbarFilters'>
+          <div className='Reset'>
             <h3>Reset!</h3>
           </div>
-          <div className="Filter">
+          <div className='Filter' id='price' onMouseEnter={this.showTooltip.bind(this)} onMouseLeave={this.hideTooltip.bind(this)}>
             <h3>Price Filter</h3>
+            <ToolTip active={this.state.isTooltipActive} parent='#price' position='bottom' arrow='left'>
+              Select Price Range!
+            </ToolTip>
           </div>
-          <div className="Filter">
+          <div className='Filter'>
             <h3>Price /M2 Filter</h3>
           </div>
-          <div className="Filter">
+          <div className='Filter'>
             <h3>Size Filter</h3>
           </div>
-          <div className="Filter">
+          <div className='Filter'>
             <h3>City Filter</h3>
           </div>
         </div>
