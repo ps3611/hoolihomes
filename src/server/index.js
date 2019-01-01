fd37af0e97aa4d4667329395df3d3944ff85b58c
Peter Schindler <petertasschindler@gmail.com>
2019-01-01 19:09:40 +0100

    refactor: cleaning

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,3 @@ app
   .use(cors())
   .use(router)
   .listen(port, () => console.log(`Express app listinging on port ${port}`));
-
-// EXAMPLE REQUEST
-// http://127.0.0.1:3001/homes?city=barcelona&centerLatitude=41.385063&centerLongitude=2.173404&radius=50000&page=2&country=es&size=[50,250]&price=[50000,5000000]&estimatedPricePercentageDifference=[-50,50]
